27fc13010d9e9fd4ad3aef4f0e22689e00b249b4
yarastqt <yarastqt@gmail.com>
2021-12-02 21:40:17 +0300

    feat: complete day 2

---
+++ day-02/dive.p1.ts
@@ -0,0 +1,36 @@
+import fs from 'fs'
+import path from 'path'
+
+const content = fs.readFileSync(path.resolve(__dirname, 'dive.input.txt'), 'utf-8')
+const steps = content
+  .split(/\n/)
+  .filter(Boolean)
+  .map((instruction) => {
+    const [direction, rawValue] = instruction.split(/\s/)
+    const value = Number(rawValue)
+
+    switch (direction) {
+      case 'forward':
+        return { x: value }
+      case 'down':
+        return { y: value }
+      case 'up':
+        return { y: -value }
+      default:
+        throw new Error(`Unexpected direction: "${direction}".`)
+    }
+  })
+
+const grid = { x: 0, y: 0 }
+
+for (const step of steps) {
+  if (step?.x) {
+    grid.x += step.x
+  } else if (step?.y) {
+    grid.y += step.y
+  }
+}
+
+const target = grid.x * grid.y
+
+console.log('>>> target', target)
